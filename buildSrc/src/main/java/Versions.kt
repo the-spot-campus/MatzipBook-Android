618695eeed8956862e8bf6837a0f9f9c3b67f53a
rudtjr1106 <rudtjr1206@naver.com>
2025-06-18 13:43:57 +0900

    feat : 라이브러리 및 버전 관리를 위한 파일 생성

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,46 @@
+// Version 관리용 object
+object Versions {
+//    const val GRADLE = "7.4.2"
+//
+//    /* [ Kotlin } */
+//    const val KOTLIN_VERSION = "1.9.24"
+//    const val K_STDLIB = "1.9.24"
+//    const val K_COROUTINES_CORE = "1.6.4"
+//    const val K_COROUTINES = "1.3.9"
+//
+//    /* [ Google ] */
+//    const val MATERIAL = "1.11.0"
+//    const val HILT = "2.46"
+//    const val HILT_COMPOSE = "1.0.0"
+//    const val GLIDE = "4.12.0"
+//    const val FCM = "24.0.0"
+//    const val FIREBASE = "32.3.1"
+//
+//    /* [ Android ] */
+//    const val CORE = "1.12.0"
+//    const val APPCOMPAT = "1.6.1"
+//    const val LIFECYCLE_KTX = "2.5.1"
+//    const val LIFECYCLE_COMPOSE = "2.6.0"
+//    const val VIEW_MODEL_KTX = "2.5.1"
+//    const val CONSTRAINT_LAYOUT = "2.1.4"
+//    const val FRAGMENT_KTX = "1.5.2"
+//    const val NAVIGATION = "2.5.3"
+//    const val REFRESH = "1.1.0"
+//    const val DATA_STORE = "1.0.0"
+//    const val ACTIVITY_COMPOSE = "1.8.2"
+//    const val COMPOSE_BOM = "2023.08.00"
+//    const val COMPOSE_MATERIAL3 = "1.1.0"
+//    const val COMPOSE_PREVIEW = "1.5.0"
+//    const val RUNTIME_LIVE_DATA = "1.5.0"
+//
+//    /* [ Libraries ] */
+//    const val RETROFIT = "2.9.0"
+//    const val OKHTTP = "4.10.0"
+//    const val PAGER = "0.28.0"
+//    const val LOTTIE = "6.5.2"
+//    const val COIL = "2.3.0"
+//    const val GRID = "2.0.0"
+//
+//    /* [ Kakao ] */
+//    const val KAKAO = "2.20.1"
+}
