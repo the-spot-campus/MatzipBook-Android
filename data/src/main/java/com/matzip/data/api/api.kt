52fd660c4e7d7a208aba8ae8772decbe9b26cb59
rudtjr1106 <rudtjr1206@naver.com>
2025-06-18 15:07:51 +0900

    feat : data 모듈 패키지 임시 생성

---
+++ data/src/main/java/com/matzip/data/api/api.kt
@@ -0,0 +1,5 @@
+package com.matzip.data.api
+
+class api {
+    //TODO 각종 API interface가 들어갈 패키지입니다
+}
---
+++ data/src/main/java/com/matzip/data/response/response.kt
@@ -0,0 +1,5 @@
+package com.matzip.data.response
+
+class response {
+    //TODO API에서 받아오는 Response Data class 들
+}
