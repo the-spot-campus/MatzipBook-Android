ac7cc7fd2c7d7ff03c8e0a9642b10cfeee755100
rudtjr1106 <rudtjr1206@naver.com>
2025-06-18 15:26:58 +0900

    feat : feature gradle 수정

--- feature/build.gradle.kts
+++ feature/build.gradle.kts
@@ -34,9 +34,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation(AndroidX.CORE)
+    implementation(AndroidX.APPCOMPAT)
+    implementation(Google.MATERIAL)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
